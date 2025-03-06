b5fcea8c53883fa5d859b26c5b11ec1331febab1
jkumar <jitendra.kumar@diona.com>
2025-03-06 13:01:44 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div>
+      <h1>Taskify</h1>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
+ 
